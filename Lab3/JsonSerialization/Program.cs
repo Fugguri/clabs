2f0529ac79c62ca78f5624da84c352181daf4d57
Fugguri <fygguri@gmail.com>
2023-12-19 11:34:37 +0300

    first commit

---
+++ Lab3/JsonSerialization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.IO;
+using Lab3;
+namespace JsonSerialization
+{
+
+    class JsonObjectSerializer<T>
+    {
+        public List<T> ReadDataFromFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<List<T>>(json);
+            }
+
+            return new List<T>();
+        }
+
+        public void WriteDataToFile(List<T> objects, string filePath)
+        {
+            string json = JsonSerializer.Serialize(objects);
+            File.WriteAllText(filePath, json);
+        }
+    }
+
+}
+
+
---
+++ Lab3/XMLSerializer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XmlSerialization
+{
+    public class XmlObjectSerializer<T>
+    {
+        public List<T> ReadDataFromFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+                using (TextReader reader = new StreamReader(filePath))
+                {
+                    return (List<T>)serializer.Deserialize(reader);
+                }
+            }
+
+            return new List<T>();
+        }
+
+        public void WriteDataToFile(List<T> objects, string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+            using (TextWriter writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, objects);
+            }
+        }
+    }
+}
